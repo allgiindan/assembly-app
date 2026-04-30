05fd43ec7ef300039fc8561f4d3bf5c325b4b21e
allgiindan <allgiindan@gmail.com>
2026-04-30 23:49:38 +0000

    Updates

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  "SUPABASE_URL": "https://jvcfxzqsavzaksddayqy.supabase.co",
+  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Y2Z4enFzYXZ6YWtzZGRheXF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1NDkwMjYsImV4cCI6MjA5MzEyNTAyNn0.p9C5wHxI5RmS7Qh0km03atNJPxQOlG-qU5rdJL-2fYM"
+};
